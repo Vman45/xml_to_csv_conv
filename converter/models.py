ac2dfe7dd6afe50e06866b74214915cefd374bc8
njh <nate.indemand@gmail.com>
2020-07-15 20:35:44 -0400

    gh commit

--- converter/models.py
+++ converter/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Request(models.Model):
+	series = models.CharField(max_length=100)
 	studio = models.CharField(max_length=100)
 	directory = models.CharField(max_length=150)
 	date_requested = models.DateField(auto_now_add=True)
+	export_csv = models.BooleanField(default=False)
+	save_csv = models.BooleanField(default=False)
+	filename = models.CharField(max_length=50, default='compiled_xmls')
 
 	#file
 	#user
--- converter/views.py
+++ converter/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Request
+from .forms import RequestForm
+
 
 def home(request):
 
-	return render(request, 'home.html', {})
+	all_requests = Request.objects.all
+
+	return render(request, 'home.html', {'requests': all_requests})
 
 
 def add_request(request):
 
-	return render(request, 'add_request.html', {})
+	if request.method == 'POST':
+		form = RequestForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Request has been made.'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Something went wrong...'))
+			return render(request, 'add_request.html', {})
+	else:
+		return render(request, 'add_request.html', {})
